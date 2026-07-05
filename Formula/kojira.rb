34859c1f7ce7ac7704ee166ffaea4d997711fa3e
Christian Haack <christian.haack@latam.com>
2026-07-05 13:42:02 -0400

    kojira 0.1.1

--- Formula/kojira.rb
+++ Formula/kojira.rb
@@ -7,22 +7,22 @@ class Kojira < Formula
   on_macos do
     on_arm do
       url "https://github.com/ragamo/kojira/releases/download/v0.1.1/kojira-aarch64-apple-darwin.tar.gz"
-      sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+      sha256 "66202318b2901ea09c5f829bdd7a491e0f2670320d896318629878cc08c295bc"
     end
     on_intel do
       url "https://github.com/ragamo/kojira/releases/download/v0.1.1/kojira-x86_64-apple-darwin.tar.gz"
-      sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+      sha256 "c398feabddbf55351e95b03b01fd092bb577dd7a6c17b86f6f480e1d0a43a852"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/ragamo/kojira/releases/download/v0.1.1/kojira-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+      sha256 "547331ff4c9752425af3cd8de91cc4758ab434f315d4e8f98af3b22be71ac9f8"
     end
     on_intel do
       url "https://github.com/ragamo/kojira/releases/download/v0.1.1/kojira-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+      sha256 "bc9def38898a971ef00a8374739b62c07356fa8b4816764063a2b30e1c8198c1"
     end
   end
 
